1e35ba02b6c731e2727f8a965cf74c85c9c59f41
ZulfiiaDitto <zulfiia.ditto@gmail.com>
2023-08-29 10:31:42 -0500

    adding stuff

--- scr/stages/data_load.py
+++ scr/stages/data_load.py
@@ -1,8 +1,9 @@
 import pandas as pd
 from dvclive import Live
 def get_data():
+  """function pulls data from url, modify if needed. 
+    Data is already saved in folder data"""
   final = pd.read_csv('https://raw.githubusercontent.com/aws-samples/aws-machine-learning-university-accelerated-tab/master/data/review/review_dataset.csv')
   final.drop('Name',axis = 1, inplace=True)
   final.fillna('Missing', inplace = True)
   final['difference'] = final['Age upon Outcome Days'] - final['Age upon Intake Days']
-  return final
